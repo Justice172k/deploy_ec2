2bf8d6027ef5ec13777bd440256c8d3b3ce90b2e
Mavis <macbook@Mavis-2.local>
2023-09-11 00:09:39 +0700

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("alo alo 1234");
+  res.send("alo alo 1234 test");
 });
 
 app.listen(process.env.PORT || 5001, () => {
