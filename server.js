54fb4d65055a2a734705e7ea14cef4a2ce4e3fa3
Mavis <macbook@Mavis-2.local>
2023-09-10 17:44:57 +0700

    test

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ require("dotenv").config();
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("alo alo alo alo");
 });
 
 app.listen(process.env.PORT || 5001, () => {
+  console.log(process.env.PORT);
   console.log("Server is running ");
 });
